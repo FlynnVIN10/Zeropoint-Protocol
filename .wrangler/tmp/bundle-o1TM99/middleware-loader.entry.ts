6185c88bc4339d51ae1f4ef3994291d5b938ad6b
hyberq <164056789+FlynnVIN10@users.noreply.github.com>
2025-08-21 11:51:39 -0500

    [PHASE5] fix routing + UI; update evidence

--- .wrangler/tmp/bundle-o1TM99/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-o1TM99/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-ogN01Z/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-o1TM99/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/usr/local/lib/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-ogN01Z/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-o1TM99/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-ogN01Z/middleware-insertion-facade.js";
+export * from "/Users/Flynn/Documents/GitHub/Zeropoint-Protocol/.wrangler/tmp/bundle-o1TM99/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
